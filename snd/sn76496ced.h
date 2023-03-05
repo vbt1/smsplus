66da6430d486b2a549b33e563ca3a82aafae70ac
vbt1 <vberthelot@free.fr>
2023-03-05 18:43:00 +0100

    adding missing files

---
+++ snd/sn76496ced.h
@@ -0,0 +1,284 @@
+#pragma once
+#include <stdint.h>
+#include <string.h>
+#include "scsp.h"
+#include "notes.h"
+
+static uint8_t sn74xx_regs[256];
+typedef struct
+{
+    uint8_t latched_chan;
+    uint8_t last_register;
+    uint16_t registers[8];
+    uint16_t period[4];
+    uint8_t vol[4];
+    uint8_t noise_type;
+} sn76496_t;
+
+static sn76496_t sn_chip_0;
+
+typedef struct
+{
+    uint8_t oct;
+    uint16_t fns;
+} sn_scsp_map_t;
+
+#include "sn_map.h"
+
+#define SN_NOTE (note_a4)
+#define SN_PER_NOTE (sn_noise_per)
+#define SN_NOISE_NOTE (sn76xxx_noise_a4)
+
+uint32_t sn_square_addr = (0x2000);
+uint32_t sn_periodic_addr = (0x2400);
+uint32_t sn_white_noise_addr = (0x3400);
+
+// Math.pow(2.0, -(x/16.0));
+const uint8_t volume_map[] = {
+    0, 5, 11, 16, 21, 26, 32, 37, 43, 48, 53, 58, 64, 69, 74, 255};
+
+//#include "tone_func.h"
+
+void sn76496_w(uint8_t dd);
+
+void sn_fix_note(uint16_t *note, uint32_t sz, uint16_t h, uint16_t l)
+{
+    for (int i = 0; i < sz; i++)
+    {
+        note[i] = note[i] & 0x8000 ? h : l;
+    }
+}
+
+void sn76496_init()
+{
+    snd_init();
+    // copy sinetable
+    for (int i = 0; i < 1; i++)
+    {
+        sn_fix_note((uint16_t *)SN_NOTE, sizeof(SN_NOTE) / 2, 0x2000, 0);
+        // sn_fix_note((uint16_t *)SN_PER_NOTE, sizeof(SN_PER_NOTE) / 2);
+        sn_fix_note((uint16_t *)SN_NOISE_NOTE, sizeof(SN_NOISE_NOTE) / 2, 0x1000, 0);
+
+        memcpy((void *)(SCSP_RAM + sn_square_addr + (i * sizeof(SN_NOTE))), SN_NOTE, sizeof(SN_NOTE));
+        memcpy((void *)(SCSP_RAM + sn_periodic_addr + (i * sizeof(SN_PER_NOTE))), SN_PER_NOTE, sizeof(SN_PER_NOTE));
+        memcpy((void *)(SCSP_RAM + sn_white_noise_addr + (i * sizeof(SN_NOISE_NOTE))), SN_NOISE_NOTE, sizeof(SN_NOISE_NOTE));
+    }
+
+    memset(sn74xx_regs, 0, 256);
+
+    volatile uint16_t *control = (uint16_t *)0x25b00400;
+    volatile uint16_t *intr = (uint16_t *)0x25b0042a;
+    control[0] = 0xf; // master vol
+    intr[0] = 0;      // irq
+
+    // init slots...
+    volatile scsp_slot_regs_t *slots = get_scsp_slot(0);
+
+    for (int i = 0; i < 32; i++)
+    {
+        slots[i].kyonb = 0;
+        slots[i].kyonex = 0;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        scsp_slot_regs_t slot = {};
+        memset(&slot, 0, sizeof(scsp_slot_regs_t));
+
+        //
+        slot.sa = sn_square_addr;
+        slot.lsa = 0;
+        slot.lea = (sizeof(SN_NOTE) / 2);
+
+        // chan 3 => noise
+        if (i == 3)
+        {
+            slot.sa = sn_white_noise_addr;
+            slot.lea = (sizeof(SN_NOISE_NOTE) / 2);
+        }
+
+        // chan 4 => per
+        if (i == 4)
+        {
+            slot.sa = sn_periodic_addr;
+            slot.lea = (sizeof(SN_PER_NOTE) / 2);
+        }
+
+        // uned 16 bit
+        slot.pcm8b = 0;
+        slot.sbctl = 0;
+
+        slot.lpctl = 1;
+        slot.attack_rate = 31;
+        slot.release_r = 31;
+        slot.loop_start = 1;
+        slot.kr_scale = 0;
+        slot.sdir = 0;
+        slot.disdl = 7;
+        slot.total_l = 0xff;
+
+        slot.fns = 0;
+        slot.oct = 0;
+
+        // memcpy(&slots[i], 0, sizeof(scsp_slot_regs_t));
+        for (int r = 0; r < 16; r++)
+        {
+            slots[i].raw[r] = slot.raw[r];
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        slots[i].kyonb = 1;
+        slots[i].kyonex = 1;
+    }
+
+    sn76496_t *chip = &sn_chip_0;
+    chip->last_register = 0;
+    for (int i = 0; i < 8; i += 2)
+    {
+        chip->registers[i] = 0;
+        chip->registers[i + 1] = 0x0f; /* volume = 0 */
+    }
+
+    // __dbg_tuneA5(&slots[0]);
+
+    // dbg...
+    if (0)
+    {
+        // channel 0 tone 0xfe 440hz
+        sn76496_w(0b10001110); // 0b1_00_0_1110
+        sn76496_w(0b00001111); // 0b0_00_0_1111
+        // set vol to max
+        sn76496_w(0b10010000); // 0b1_00_1_0000
+    }
+
+    // periodic noise
+    if (0)
+    {
+        // use periodic noise
+        sn76496_w(0b11100110); // 0b1_11_0_0000
+        // set vol to max
+        sn76496_w(0b11110000); // 0b1_11_1_0000
+    }
+}
+
+void sn76496_w(uint8_t dd)
+{
+    // return;
+    volatile scsp_slot_regs_t *slots = get_scsp_slot(0);
+    sn76496_t *chip = &sn_chip_0;
+
+    // // emu_printf("sn76496_w %02x\n", dd);
+
+    uint16_t r = 0;
+    uint16_t n = 0;
+    uint8_t chan = 0;
+
+    if ((dd & 0x80) == 0x80)
+    {
+        r = (dd & 0x70) >> 4;
+        chip->last_register = r;
+        chip->latched_chan = chan;
+
+        chip->registers[r] = (chip->registers[r] & 0x3f0) | (dd & 0x0f);
+    }
+    else
+    {
+        r = chip->last_register;
+    }
+
+    chan = (r >> 1) & 3;
+    switch (r)
+    {
+        // tone: frequency
+    case 0:
+    case 2:
+    case 4:
+        if ((dd & 0x80) == 0)
+            chip->registers[r] = (chip->registers[r] & 0x0f) | ((dd & 0x3f) << 4);
+        if (chip->registers[r] != 0)
+            chip->period[chan] = chip->registers[r] & 0x3ff;
+        else
+            chip->period[chan] = 0x400;
+        break;
+        // noise: frequency, mode
+    case 6:
+        n = chip->registers[6];
+        chip->noise_type = n & 4;
+        if ((dd & 0x80) == 0)
+            chip->registers[r] = (chip->registers[r] & 0x3f0) | (dd & 0x0f);
+        // N/512,N/1024,N/2048,Tone #3 output
+        chip->period[3] = ((n & 3) == 3) ? (chip->period[2] << 1) : (1 << (5 + (n & 3)));
+        break;
+        // tone/noise: volume
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+        chip->vol[chan] = dd & 0x0f;
+        if ((dd & 0x80) == 0)
+            chip->registers[r] = (chip->registers[r] & 0x3f0) | (dd & 0x0f);
+
+        // emu_printf("sn76496_w vol chan %x %d\n", chip->vol[chan], chan);
+        break;
+    }
+
+    // if (chan < 3) // skip noise
+    {
+
+        if (chan == 3)
+        {
+            // return;
+            volatile scsp_slot_regs_t *slot_noise = &slots[3];
+            volatile scsp_slot_regs_t *slot_per = &slots[4];
+            volatile scsp_slot_regs_t *slot = chip->noise_type ? slot_noise : slot_per;
+
+            slot->kyonb = 0;
+            slot->kyonex = 0;
+
+            if (chip->noise_type)
+            {
+                slot_per->total_l = 0xff;
+                slot_per->kyonb = 0;
+                slot_per->kyonex = 0;
+            }
+            else
+            {
+                slot_noise->total_l = 0xff;
+                slot_noise->kyonb = 0;
+                slot_noise->kyonex = 0;
+            }
+
+            slot->fns = sn_scsp_map[chip->period[chan]].fns;
+            slot->oct = sn_scsp_map[chip->period[chan]].oct;
+            slot->total_l = volume_map[chip->vol[chan]];
+
+            // need to wait for reset ?
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+
+            slot->kyonb = 1;
+            slot->kyonex = 1;
+
+            // emu_printf("sn76496_w noise per %x\n", chip->period[chan]);
+            // emu_printf("sn76496_w noise fns %d\n", sn_scsp_map[chip->period[chan]].fns);
+            // emu_printf("sn76496_w noise oct %d\n", sn_scsp_map[chip->period[chan]].oct);
+            // emu_printf("sn76496_w noise vol %d\n", volume_map[chip->vol[chan]]);
+        }
+        else
+        {
+            // return;
+            //* return;
+            slots[chan].fns = sn_scsp_map[chip->period[chan]].fns;
+            slots[chan].oct = sn_scsp_map[chip->period[chan]].oct;
+            slots[chan].total_l = volume_map[chip->vol[chan]];
+            // slots[chan].total_l = chip->vol[chan];
+
+            // slots[chan].kyonb = 1;
+            // slots[chan].kyonex = 1;
+        }
+    }
+}
---
+++ snd/vgm.c
@@ -0,0 +1,342 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <yaul.h>
+#include "vgm.h"
+#include "sinepatt.h"
+#include "scsp.h"
+#include "tr.h"
+//#include "empty_drv.h"
+
+//
+void emu_printf(const char *format, ...)
+{
+   static char emu_printf_buffer[256];
+   char *s = emu_printf_buffer;
+   volatile uint8_t *addr = (volatile uint8_t *)CS1(0x1000);
+   va_list args;
+
+   va_start(args, format);
+   (void)vsnprintf(emu_printf_buffer, 256, format, args);
+   va_end(args);
+
+   while (*s)
+      *addr = (uint8_t)*s++;
+}
+
+#include "notes.h"
+
+void snd_init();
+
+// chips
+#include "./ym2151.h"
+#include "./ym2203.h"
+#include "./sn76496.h"
+
+void snd_init()
+{
+   *(volatile uint8_t *)(0x25B00400) = 0x02;
+   // Turn off Sound CPU
+   smpc_smc_sndoff_call();
+
+   // Make sure SCSP is set to 512k mode
+   *(volatile uint8_t *)(0x25B00400) = 0x02;
+
+   // Clear Sound Ram
+   for (int i = 0; i < 0x80000; i += 4)
+      *(volatile uint32_t *)(0x25A00000 + i) = 0x00000000;
+
+   // Copy driver over
+//   for (int i = 0; i < sound_driver_size; i++)
+//      *(volatile uint8_t *)(0x25A00000 + i) = sound_driver[i];
+	GFS_Load(GFS_NameToId("SDRV.BIN"),0,(void *)0x25A00000,sound_driver_size);
+
+   // Turn on Sound CPU again
+   smpc_smc_sndon_call();
+}
+
+#define SAMPLING_RATE 44100
+
+uint8_t get_vgm_ui8(vgm_player_t *vgm_player)
+{
+   return vgm_player->vgm[vgm_player->vgmpos++];
+}
+
+uint16_t get_vgm_ui16(vgm_player_t *vgm_player)
+{
+   return get_vgm_ui8(vgm_player) + (get_vgm_ui8(vgm_player) << 8);
+}
+
+uint32_t get_vgm_ui32(vgm_player_t *vgm_player)
+{
+   return get_vgm_ui8(vgm_player) + (get_vgm_ui8(vgm_player) << 8) + (get_vgm_ui8(vgm_player) << 16) + (get_vgm_ui8(vgm_player) << 24);
+}
+
+void vgm_restart(vgm_player_t *vgm_player)
+{
+   vgm_player->vgmpos = 0x34;
+   uint32_t offset = get_vgm_ui32(vgm_player);
+   if (offset == 0)
+   {
+      vgm_player->vgmpos = 0x40;
+   }
+}
+
+uint16_t vgm_parse(vgm_player_t *vgm_player)
+{
+   uint8_t command;
+   uint16_t wait = 0;
+   uint8_t reg;
+   uint8_t dat;
+
+   command = get_vgm_ui8(vgm_player);
+   switch (command)
+   {
+      /*
+      AY8910 stereo mask, dd is a bit mask of i y r3 l3 r2 l2 r1 l1 (bit 7 ... 0)
+      i	chip instance (0 or 1)
+      y	set stereo mask for YM2203 SSG (1) or AY8910 (0)
+      l1/l2/l3	enable channel 1/2/3 on left speaker
+      r1/r2/r3	enable channel 1/2/3 on right speaker
+      */
+   case 0x31:
+      dat = get_vgm_ui8(vgm_player);
+      // ignored
+      break;
+
+      // SN76489/SN76496
+   case 0x4f: // Game Gear PSG stereo, write dd to port 0x06
+   case 0x50: // 0x50	dd	PSG (SN76489/SN76496) write value dd
+      dat = get_vgm_ui8(vgm_player);
+      sn76496_w(dat);
+      break;
+      // YM2612 port 0 - 1
+   case 0x52:
+   case 0x53:
+      reg = get_vgm_ui8(vgm_player);
+      dat = get_vgm_ui8(vgm_player);
+      break;
+      // 0x54	aa dd YM2151, write value dd to register aa
+   case 0x54:
+      reg = get_vgm_ui8(vgm_player);
+      dat = get_vgm_ui8(vgm_player);
+      ym2151_w(reg, dat);
+      break;
+      // 0x55	aa dd	YM2203, write value dd to register aa
+   case 0x55:
+      reg = get_vgm_ui8(vgm_player);
+      dat = get_vgm_ui8(vgm_player);
+      ym2203_w(reg, dat);
+      break;
+
+      // wait
+   case 0x61:
+      wait = get_vgm_ui16(vgm_player);
+      break;
+   case 0x62:
+      wait = 735;
+      break;
+   case 0x63:
+      wait = 882;
+      break;
+   case 0x66:
+      if (vgm_player->vgmloopoffset == 0)
+      {
+         vgm_player->vgmend = 1;
+      }
+      else
+      {
+         vgm_player->vgmpos = vgm_player->vgmloopoffset;
+         vgm_player->vgmpos++;
+      }
+      break;
+   case 0x67: //  0x67 0x66 tt ss ss ss ss (data)
+
+   {
+      get_vgm_ui8(vgm_player); // 0x66
+      uint8_t tt = get_vgm_ui8(vgm_player);
+      uint32_t size = get_vgm_ui32(vgm_player);
+      uint8_t *data_start = vgm_player->vgm + vgm_player->vgmpos;
+
+      uint32_t chunk_pos = vgm_player->vgmpos;
+      switch (tt)
+      {
+      case 0x8B:
+         uint32_t rom_size = get_vgm_ui32(vgm_player);
+         uint32_t rom_off = get_vgm_ui32(vgm_player);
+         uint32_t rom_len = size - 8;
+         dbgio_printf("oki m6295 data %04x %04x %04x\n", rom_size, rom_off, rom_len);
+         break;
+      default:
+         break;
+      }
+
+      vgm_player->datpos = vgm_player->vgmpos;
+      vgm_player->vgmpos = chunk_pos + size;
+   }
+   break;
+   case 0x70:
+   case 0x71:
+   case 0x72:
+   case 0x73:
+   case 0x74:
+   case 0x75:
+   case 0x76:
+   case 0x77:
+   case 0x78:
+   case 0x79:
+   case 0x7a:
+   case 0x7b:
+   case 0x7c:
+   case 0x7d:
+   case 0x7e:
+   case 0x7f:
+      wait = (command & 0x0f) + 1;
+      break;
+   case 0x80:
+   case 0x81:
+   case 0x82:
+   case 0x83:
+   case 0x84:
+   case 0x85:
+   case 0x86:
+   case 0x87:
+   case 0x88:
+   case 0x89:
+   case 0x8a:
+   case 0x8b:
+   case 0x8c:
+   case 0x8d:
+   case 0x8e:
+   case 0x8f:
+      wait = (command & 0x0f);
+      //   YM2612_Write(0, 0x2a);
+      //   YM2612_Write(1, vgm_player->vgm[vgm_player->datpos + vgm_player->pcmpos + vgm_player->pcmoffset]);
+      vgm_player->pcmoffset++;
+      break;
+
+   case 0xB8:                  // 0xB8	aa dd	OKIM6295, write value dd to register aa
+      get_vgm_ui8(vgm_player); // 0x66
+      get_vgm_ui8(vgm_player); // 0x00 data type
+      break;
+   case 0xe0:
+      vgm_player->pcmpos = get_vgm_ui32(vgm_player);
+      vgm_player->pcmoffset = 0;
+      break;
+   default:
+      dbgio_printf("unknown cmd at 0x%x: 0x%x\n", vgm_player->vgmpos, command);
+      vgm_player->vgmpos++;
+      vgm_restart(vgm_player);
+      break;
+   }
+
+   // dbgio_printf("command: %x\nwait:%d\n", command, wait);
+
+   if (vgm_player->vgmend)
+   {
+      vgm_player->pcmpos = 0x34;
+      vgm_player->vgmpos = 0x34 + get_vgm_ui32(vgm_player);
+      vgm_player->vgmend = 0;
+   }
+   vgm_player->cycles += wait;
+   return wait;
+}
+
+static void vgm_parse_header(vgm_player_t *vgm_player)
+{
+   // vgm_player->vgmpos = 0x40;
+   vgm_player->vgmend = 0;
+
+   // parse header
+   vgm_player->vgmpos = 0x44;
+   vgm_player->clock_ym2203 = get_vgm_ui32(vgm_player);
+
+   vgm_player->vgmpos = 0x30;
+   vgm_player->clock_ym2151 = get_vgm_ui32(vgm_player);
+
+   vgm_player->vgmpos = 0x0C;
+   vgm_player->clock_sn76 = get_vgm_ui32(vgm_player);
+
+   vgm_player->vgmpos = 0x1c;
+   vgm_player->vgmloopoffset = get_vgm_ui32(vgm_player);
+
+   vgm_player->vgmpos = 0x34;
+   uint32_t offset = get_vgm_ui32(vgm_player);
+   if (offset == 0)
+   {
+      vgm_player->vgmpos = 0x40;
+   }
+   else
+   {
+      vgm_player->vgmpos = 0x34 + offset;
+   }
+
+   if (vgm_player->clock_ym2151)
+      ym2151_init();
+
+   if (vgm_player->clock_ym2203)
+      ym2203_init();
+
+   if (vgm_player->clock_sn76)
+      sn76496_init();
+
+   if (vgm_player->clock_ym2203 == 0)
+      vgm_player->clock_ym2203 = 3000000; // 4000000;
+}
+
+/* C:\Users\cc\Downloads\DefleMask_Legacy_Windows (1)\a4_sms.vgm (05/02/2023 12:43:21)
+   DébutPosition(h): 00000000, FinPosition(h): 000000E7, Longueur(h): 000000E8 */
+
+unsigned char a4_sms[232] = {
+    0x56, 0x67, 0x6D, 0x20, 0xE4, 0x00, 0x00, 0x00, 0x50, 0x01, 0x00, 0x00,
+    0x99, 0x9E, 0x36, 0x00, 0x00, 0x00, 0x00, 0x00, 0x44, 0x00, 0x00, 0x00,
+    0x40, 0x27, 0x02, 0x00, 0x26, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x3C, 0x00, 0x00, 0x00, 0x09, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x50, 0x9F, 0x50, 0xBF, 0x50, 0xDF, 0x50, 0xFF,
+    0x50, 0x90, 0x50, 0x80, 0x50, 0x04, 0x61, 0xFF, 0xFF, 0x61, 0xFF, 0xFF,
+    0x61, 0x42, 0x27, 0x66, 0x47, 0x64, 0x33, 0x20, 0x00, 0x01, 0x00, 0x00,
+    0x84, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x53, 0x00, 0x65, 0x00, 0x67, 0x00, 0x61, 0x00, 0x20, 0x00, 0x4D, 0x00,
+    0x61, 0x00, 0x73, 0x00, 0x74, 0x00, 0x65, 0x00, 0x72, 0x00, 0x20, 0x00,
+    0x53, 0x00, 0x79, 0x00, 0x73, 0x00, 0x74, 0x00, 0x65, 0x00, 0x6D, 0x00,
+    0x20, 0x00, 0x2F, 0x00, 0x20, 0x00, 0x47, 0x00, 0x61, 0x00, 0x6D, 0x00,
+    0x65, 0x00, 0x20, 0x00, 0x47, 0x00, 0x65, 0x00, 0x61, 0x00, 0x72, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x44, 0x00,
+    0x65, 0x00, 0x6C, 0x00, 0x65, 0x00, 0x6B, 0x00, 0x27, 0x00, 0x73, 0x00,
+    0x20, 0x00, 0x44, 0x00, 0x65, 0x00, 0x66, 0x00, 0x6C, 0x00, 0x65, 0x00,
+    0x4D, 0x00, 0x61, 0x00, 0x73, 0x00, 0x6B, 0x00, 0x20, 0x00, 0x54, 0x00,
+    0x72, 0x00, 0x61, 0x00, 0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x72, 0x00,
+    0x00, 0x00, 0x00, 0x00};
+
+int vgm_init(vgm_player_t *vgm_player)
+{
+   dbgio_init();
+   dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC);
+   dbgio_dev_font_load();
+
+   smpc_smc_sndon_call();
+
+   // vgm_player->vgm = debug_2151_vgm;
+   // vgm_player->vgm = atomic_kid;
+   // vgm_player->vgm = ys_vgm;
+   // vgm_player->vgm = sf2_ryu;
+   // vgm_player->vgm = sf2_title;
+   vgm_player->vgm = sonic_sn76496;
+   // vgm_player->vgm = a4_sms;
+   vgm_parse_header(vgm_player);
+
+   vgm_player->sampled = 0;
+   vgm_player->frame_size = -1;
+   vgm_player->sample_count = 0;
+   vgm_player->cycles = 0;
+   vgm_player->cycles_played = 0;
+
+   emu_printf("vgm_player->vgmloopoffset 0x%x\n", vgm_player->vgmloopoffset);
+   dbgio_printf("vgm_player->vgmloopoffset 0x%x\n", vgm_player->vgmloopoffset);
+
+   return 0;
+}
+}
